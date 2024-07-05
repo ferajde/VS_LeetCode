3708183c0b7effc9d58454a3cd01390b1c1274e0
ferajde <111061839+ferajde@users.noreply.github.com>
2024-07-05 13:19:55 +0200

    first commit

---
+++ 1.two-sum.cs
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1 lang=csharp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        
+        for (int i=1;i<nums.Length();i++){
+            
+        }
+    }
+}
+// @lc code=end
+
